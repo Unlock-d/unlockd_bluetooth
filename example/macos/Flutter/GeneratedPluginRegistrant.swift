a984856a84ce975b7c8bb4de310e60261228d532
Pieter-Jan Robrecht <pieter-jan.robrecht@xplore.be>
2024-02-09 13:09:45 +0100

    Clean up unnecessary dependencies

--- example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,16 +5,12 @@
 import FlutterMacOS
 import Foundation
 
-import device_info_plus
 import flutter_blue_plus
 import isar_flutter_libs
 import nordic_dfu
-import path_provider_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
   FlutterBluePlusPlugin.register(with: registry.registrar(forPlugin: "FlutterBluePlusPlugin"))
   IsarFlutterLibsPlugin.register(with: registry.registrar(forPlugin: "IsarFlutterLibsPlugin"))
   NordicDfuPlugin.register(with: registry.registrar(forPlugin: "NordicDfuPlugin"))
-  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
 }
