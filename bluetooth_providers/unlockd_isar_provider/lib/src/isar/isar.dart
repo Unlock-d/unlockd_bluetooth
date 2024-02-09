80a7715d6410083baf663e27b071edddf2a18bcc
Pieter-Jan Robrecht <pieter-jan.robrecht@xplore.be>
2024-02-09 11:33:49 +0100

    Rename isar_storage_provider to isar_provider

--- bluetooth_providers/unlockd_isar_provider/lib/src/isar/isar.dart
+++ bluetooth_providers/unlockd_isar_provider/lib/src/isar/isar.dart
@@ -3,13 +3,13 @@ import 'dart:async';
 import 'package:isar/isar.dart';
 import 'package:unlockd_bluetooth_core/unlockd_bluetooth.dart';
 
+part '../isar_bluetooth_provider.dart';
 part 'isar.g.dart';
 part 'isar_advertisement_data.dart';
 part 'isar_bluetooth_characteristic.dart';
 part 'isar_bluetooth_config.dart';
 part 'isar_bluetooth_descriptor.dart';
 part 'isar_bluetooth_device.dart';
-part '../isar_bluetooth_provider.dart';
 part 'isar_bluetooth_service.dart';
 part 'isar_characteristic_properties.dart';
 part 'isar_manufacturer_data.dart';
---
+++ bluetooth_providers/unlockd_isar_provider/lib/unlockd_isar_storage_provider.dart
@@ -0,0 +1,4 @@
+/// A Unlock'd Flutter package created by Unlock'd.
+library unlockd_isar_provider;
+
+export 'package:unlockd_isar_provider/src/isar/isar.dart';
