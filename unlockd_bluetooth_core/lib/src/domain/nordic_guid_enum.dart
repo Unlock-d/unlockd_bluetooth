d2ce4b3bc27434bf9107cc23646e1d20585a17da
Pieter-Jan Robrecht <pieter-jan.robrecht@xplore.be>
2024-02-27 15:02:44 +0100

    (feat) Add UART service to Nordic GUID enum

--- unlockd_bluetooth_core/lib/src/domain/nordic_guid_enum.dart
+++ unlockd_bluetooth_core/lib/src/domain/nordic_guid_enum.dart
@@ -7,6 +7,11 @@ enum NordicGuidEnum {
   BATTERY_SERVICE('180f', NORDIC_MASK),
   BUTTONLESS_DFU_SERVICE('fe59', NORDIC_MASK),
 
+  /// The Bluetooth® Low Energy (LE) GATT Nordic UART Service is a custom
+  /// service that receives and writes data and serves as a bridge to the
+  /// UART interface.
+  NORDIC_UART_SERVICE('1', NORDIC_UART_SERVICE_MASK),
+
   // DEVICE_INFO_SERVICE characteristics
   MANUFACTURER_NAME_CHAR('2a29', NORDIC_MASK),
   MODEL_NUMBER_CHAR('2a24', NORDIC_MASK),
@@ -17,6 +22,10 @@ enum NordicGuidEnum {
 
   // BATTERY_SERVICE characteristics
   BATTERY_LEVEL_CHAR('2a19', NORDIC_MASK),
+
+  // NORDIC_UART_SERVICE characteristics
+  NORDIC_UART_RX_CHAR('2', NORDIC_UART_SERVICE_MASK),
+  NORDIC_UART_TX_CHAR('3', NORDIC_UART_SERVICE_MASK),
   ;
 
   const NordicGuidEnum(
@@ -24,6 +33,8 @@ enum NordicGuidEnum {
     this._mask,
   );
 
+  static const NORDIC_UART_SERVICE_MASK =
+      '6e40000x-b5a3-f393-e0a9-e50e24dcca9e';
   static const SPECIAL_NORDIC_MASK = '0000xxxx-1212-efde-1523-785feabcd123';
   static const NORDIC_MASK = '0000xxxx-0000-1000-8000-00805f9b34fb';
 
